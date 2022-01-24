e3fb06763fb4754ceb09d43aeea565bea1a2a874
Alex-dll <alexsandrojr.303@gmail.com>
2022-01-24 14:02:37 -0300

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import { Login } from '../components/Login';
+import GlobalStyles from '../styles/global';
+
+export default function HomePage() {
+  return (
+    <>
+      <GlobalStyles />
+      <Login />
+    </>
+  )
+}
