30c17cd54e17eca43d4aef0e71468f12d074f3b6
Alex-dll <alexsandrojr.303@gmail.com>
2022-01-26 14:02:34 -0300

    add 404 error page

---
+++ src/pages/404.js
@@ -0,0 +1,67 @@
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { useRouter } from 'next/router';
+
+import appConfig from '../styles/appConfig.json';
+
+import { Titulo } from '../components/Titulo';
+
+
+export default function Custom404() {
+  const router = useRouter()
+
+  return (
+    <>
+      <Box
+        styleSheet={{
+          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundImage: 'url(https://images.unsplash.com/photo-1613376023733-0a73315d9b06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)' || 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+        }}
+      >
+        <Box
+          styleSheet={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: {
+              xs: 'column',
+              sm: 'row',
+            },
+            width: '100%', maxWidth: '700px',
+            borderRadius: '5px', padding: '32px', margin: '16px',
+            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+            backgroundColor: appConfig.theme.colors.primary[900],
+          }}
+        >
+          <Box
+            as="form"
+            onSubmit={function (eventInfo) {
+              eventInfo.preventDefault();
+              router.push('/')
+            }}
+            styleSheet={{
+              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+            }}
+          >
+            <Titulo tag="h2">Vish, parece que voce se perdeu em!</Titulo>
+
+            <Button
+              type='submit'
+              label='SAIR DO ERRO 404'
+              fullWidth
+              styleSheet={{ marginTop: '20px' }}
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["000"],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[600],
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </>
+  )
+}
